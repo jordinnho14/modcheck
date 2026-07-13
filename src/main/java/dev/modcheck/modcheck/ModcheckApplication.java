0b954b48279dd98eb4eb64e703b734dc609aa457
Jordan Thompson <jordan_thompson14@hotmail.co.uk>
2026-07-13 18:58:28 +0100

    Bootstrap Spring Boot 4 project

---
+++ src/main/java/dev/modcheck/modcheck/ModcheckApplication.java
@@ -0,0 +1,13 @@
+package dev.modcheck.modcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModcheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModcheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/modcheck/modcheck/ModcheckApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.modcheck.modcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModcheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
